36f9368bbc58a7f32965ae716b7606da11c0af28
DuckyCodez <Kian.Rachlin@Icloud.com>
2025-05-15 20:12:43 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,83 @@
-new Q5();
+let spawner, circlesGroup, player;
 
-new Canvas();
-world.gravity.y = 10;
 
-let ball = new Sprite();
-ball.x = halfWidth - 200;
-ball.y = halfHeight - 200;
-ball.diameter = 50;
 
-let groundA = new Sprite();
-groundA.x = halfWidth - 120;
-groundA.width = 200;
-groundA.rotation = 20;
-groundA.physics = STATIC;
+function setup() {
 
-let groundB = new Sprite();
-groundB.x = halfWidth + 120;
-groundB.width = 200;
-groundB.rotation = -20;
-groundB.physics = STATIC;
+    //  --  Canvas  -- \\
+    createCanvas(1250, 500);
+    world.gravity.y = 10;
+    
+    
+    
+    //   --   Groups   --   \\
+    
+    //  --  Circle Group  -- \\
+    circlesGroup = new Group();
+    circlesGroup.diameter = 100; 
+    circlesGroup.color = 'red';
+    circlesGroup.collider = 'static';
+    
+    
+    
+    //   -- Sprites --   \\
+    
+    //  --  Circle Spawner  --  \\
+    spawner = new Sprite();
+    spawner.height = height / 1.75;
+    spawner.width = width / 1.75;
+    spawner.y = height / 2;
+    spawner.color = 'white';
+    spawner.collider = 'static';
+    
+    
+    //  --  Player  --  \\
+    player = new Sprite();
+    player.diameter = 20;
+    player.color = 'red';
+    player.overlaps(circlesGroup, collect);
+}
 
-function update() {
-	background('skyblue');
+var moneyGain = 1;
+var money = 0;
+var upgrade = 1;
+var moneyNeeded = 400;
+var test = 0.5;
+var max_money = 100000;
 
-	textAlign(CENTER);
-	textSize(20);
-	text('click to jump!', halfWidth, halfHeight - 100);
+function draw() {
+    
+    clear();
+    background('gray');
+    player.layer = 1000;
 
-	if (mouse.presses()) ball.vel.y = -5;
+    circle_spawning()
+    
+}
+
+
+
+
+function collect(player, circle) {
+    if (mode == 'circle_spawning'){
+        circle.remove();
+        money += 1;
+    }
+};
+
+//  --  SPAWN  --  \\
+
+   //  circle  \\  
+function circle_spawning() {
+    
+    background('gray');
+    
+    if (circlesGroup.length <= 65) { 
+        circle = new circlesGroup.Sprite();
+        circle.x = random((width / 2) - (width / 4),(width / 2) + (width / 4));
+        circle.y = random((height / 2) - (height / 4),(height / 2) + (height / 4));
+    }
+    
+    textSize(50);
+    text("Money: " +round(money), 20, 60);
 }
